5aa3e28c56ef3a0c28b5dd8bc6379b5be66b4bec
ItaloAlulas <italoalulas.dev@gmail.com>
2026-01-10 21:52:48 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const botaoMenu = document.querySelector('.menu')
+const navegacao = document.querySelector('.navegacao')
+const textoDoHero = document.querySelector('.texto-do-hero')
+const ctaDentroDoHeader = document.querySelector('.cta-dentro-do-header')
+const hero = document.querySelector('.hero')
+
+botaoMenu.addEventListener('click', () => {
+    botaoMenu.classList.toggle('aberto')
+    navegacao.classList.toggle('active');
+    textoDoHero.classList.toggle('active')
+    hero.classList.toggle('active');
+    ctaDentroDoHeader.classList.toggle('hidden')
+})
